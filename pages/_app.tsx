ba454dbdbce42199ab01f8949c76a414329a7cdb
harith2001 <danula243@gmail.com>
2025-12-08 12:09:59 +0530

    tiktok icon added to footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -49,7 +49,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
                     <div className="flex items-center gap-3 text-black/70">
                       <a href="#" aria-label="Facebook" className="hover:text-gold"><svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor"><path d="M22 12a10 10 0 10-11.5 9.95v-7.04H7.9V12h2.6V9.8c0-2.56 1.52-3.97 3.84-3.97 1.11 0 2.27.2 2.27.2v2.5h-1.28c-1.26 0-1.65.78-1.65 1.58V12h2.81l-.45 2.91h-2.36v7.04A10 10 0 0022 12z"/></svg></a>
                       <a href="#" aria-label="Instagram" className="hover:text-gold"><svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor"><path d="M7 2h10a5 5 0 015 5v10a5 5 0 01-5 5H7a5 5 0 01-5-5V7a5 5 0 015-5zm10 2H7a3 3 0 00-3 3v10a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3zm-5 3a5 5 0 110 10 5 5 0 010-10zm6-1a1 1 0 110 2 1 1 0 010-2z"/></svg></a>
-                      <a href="#" aria-label="TikTok" className="hover:text-gold"><svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor"><path d="M22.46 6c-.77.35-1.6.58-2.46.69a4.27 4.27 0 00-7.3 3.9 12.13 12.13 0 01-8.8-4.47 4.27 4.27 0 001.32 5.7c-.66-.02-1.28-.2-1.83-.5v.05a4.28 4.28 0 003.42 4.19c-.62.17-1.28.2-1.92.07a4.28 4.28 0 004 2.97A8.57 8.57 0 012 19.54a12.09 12.09 0 006.56 1.92c7.88 0 12.2-6.53 12.2-12.2l-.01-.56A8.7 8.7 0 0022.46 6z"/></svg></a>
+                      <a href="#" aria-label="TikTok" className="hover:opacity-80">
+                        <img src="/tiktok.svg" alt="TikTok" className="w-5 h-6" />
+                      </a>
                     </div>
                   </div>
                 </div>
